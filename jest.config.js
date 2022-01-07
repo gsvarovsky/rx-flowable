ff29c27f15c3fdc75565c2d1a422bcfae427a413
George Svarovsky <gsvarovsky@users.noreply.github.com>
2022-01-07 17:36:10 +0000

    Extracted from m-ld-js project

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: [
+    '<rootDir>/src/',
+    '<rootDir>/test/'
+  ],
+  testMatch: [
+    '<rootDir>/test/**/*.test.ts'
+  ]
+};
